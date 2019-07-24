f338684a2159b19d688b5cf441fa3723c17f0eeb
Luis Gerardo Davila Lopez <ldavila@anzen.com.mx>
2019-07-24 16:42:12 -0500

    Practica 6

---
+++ 06-webserver/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = proccess.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'luis Gerardo'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        nombre: 'Gumball'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`);
+});
