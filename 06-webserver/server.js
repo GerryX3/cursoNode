6377e66f3ef0ed58599cc06d7c0237ae3e1c7806
Luis Gerardo Davila Lopez <ldavila@anzen.com.mx>
2019-07-25 07:54:15 -0500

    practica 7

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -3,7 +3,7 @@ const app = express();
 const hbs = require('hbs');
 require('./hbs/helpers');
 
-const port = proccess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname + '/views/parciales');
